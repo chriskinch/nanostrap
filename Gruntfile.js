ad7892fe97ddb19307f430711ab7d6460bc2c4b1
Chris Kinch <kinchie@gmail.com>
2014-07-29 21:12:14 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    meta: {
+      banner: {
+        dist: '/*!\n'+
+               ' * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>\n'+
+               ' */\n'
+      },
+      outputDir: 'js/dist',
+      output : '<%= meta.outputDir %>/<%= pkg.name %>',
+      outputMin : '<%= meta.outputDir %>/<%= pkg.name.replace("js", "min.js") %>'
+    },
+
+    concat: {
+      options: {
+        separator: ''
+      },
+      dist: {
+        src: [
+          'js/src/*.js',
+        ],
+        dest: '<%= meta.output %>.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          banner: '<%= meta.banner.dist %>'
+        },
+        files: {
+          '<%= meta.outputDir %>/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      },
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', 'js/src/']
+    },
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['test','concat','uglify']);
+
+};
